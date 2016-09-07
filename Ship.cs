6c3c4bea5844df4145d07dc6f4e72fc699039176
Mathieu Lazerges <m.lazerges@gmail.com>
2016-09-07 06:03:09 +0200

    Ajout: object 'Ship'
    Implémentation basique de l'objet 'Ship' : 4 classes instanciées.
    ("Nom", "Classe", Cargo, Prix, Entretient/Tour, "Descr. Capacité",
    "Descr. Ship")

---
+++ Ship.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceConsole
+{
+    // Classe de l'Objet 'Ship' (certainement laborieux, mais ça marche)
+    // Contient des parametres pas encore utilisés.
+    class Ship
+    {
+        //private object shipID;
+        private object shipName;        // Nom unique
+        private object shipClass;       // Type de vaisseau
+        private object shipCargo;       // Capacité
+        private object shipPrice;       // Cout du Vaisseau à l'achat
+        private object shipMaintCost;   // Cout de Maintenance / Tour
+        private object shipCargoStr;    // Description du Tonnage
+        private object shipDescr;       // Description du Vaisseau
+
+        // public Ship(int i, string n, string cl, int ca, int p, int mc, string cs, string d)
+        public Ship(string n, string cl, int ca, int p, int mc, string cs, string d)
+        {
+            //this.shipID = i;
+            this.shipName = n;
+            this.shipClass = cl;
+            this.shipCargo = ca;
+            this.shipPrice = p;
+            this.shipMaintCost = mc;
+            this.ShipCargoStr = cs;
+            this.shipDescr = d;
+        }
+        /*
+        public object ShipID
+        {
+            get { return this.shipID; }
+            set { this.shipID = value; }
+        }
+        */
+        public object ShipName
+        {
+            get { return this.shipName; }
+            set { this.shipName = value; }
+        }
+        public object ShipClass
+        {
+            get { return this.shipClass; }
+            set { this.shipClass = value; }
+        }
+        public object ShipCargo
+        {
+            get { return this.shipCargo; }
+            set { this.shipCargo = value; }
+        }
+        public object ShipPrice
+        {
+            get { return this.shipPrice; }
+            set { this.shipPrice = value; }
+        }
+        public object ShipMaintCost
+        {
+            get { return this.shipMaintCost; }
+            set { this.shipMaintCost = value; }
+        }
+        public object ShipCargoStr
+        {
+            get { return this.shipCargoStr; }
+            set { this.shipCargoStr = value; }
+        }
+        public object ShipDescr
+        {
+            get { return this.shipDescr; }
+            set { this.shipDescr = value; }
+        }
+    }
+}
